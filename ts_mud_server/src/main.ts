df8bc44014fc4e9b115e02bb873158a6e3ad9b53
cowboy8625 <cowboy8625@yahoo.com>
2023-09-30 19:29:16 -0500

    players can log in and out and back in

--- ts_mud_server/src/main.ts
+++ ts_mud_server/src/main.ts
@@ -24,6 +24,7 @@ type ClientState = {
 let offline: Player[] = [];
 let online: Socket[] = [];
 
+
 function createClient(client: Socket, clientState: ClientState) {
     client.setEncoding('utf8');
     client.write('Welcome to the chat server!\r\n');
@@ -32,14 +33,19 @@ function createClient(client: Socket, clientState: ClientState) {
     client.on('end', () => handleEnd(client, clientState));
 }
 
+
 function handleData(client: Socket, data: Buffer, clientState: ClientState) {
+  if (data.toString().charCodeAt(0) === 65533) {
+    client.end();
+    return;
+  }
   switch (clientState.state) {
     case STATE.GetName: {
       getUserName(client, data, clientState);
       break;
     }
     case STATE.CheckPassWord: {
-      throw new Error("checkPassword funtion not create yet")
+      checkPassword(client, data, clientState);
       break;
     }
     case STATE.CheckToCreateUser: {
@@ -57,6 +63,7 @@ function handleData(client: Socket, data: Buffer, clientState: ClientState) {
   }
 }
 
+
 function getUserName(client: Socket, data: Buffer, clientState: ClientState): void {
   const name = data.toString().trim();
   const index = offline.findIndex(p => p.name === name);
@@ -65,6 +72,9 @@ function getUserName(client: Socket, data: Buffer, clientState: ClientState): vo
   if (failed) {
     client.write('Looks like your not in the system.\r\n')
     client.write('Do you want to create a character?: ');
+    let player = new Player();
+    player.name = name;
+    clientState.player = Option.some(player);
     clientState.state = STATE.CheckToCreateUser;
     return;
   }
@@ -72,8 +82,29 @@ function getUserName(client: Socket, data: Buffer, clientState: ClientState): vo
   const [player, ..._] = offline.splice(index);
   online.push(client);
   clientState.player = Option.some(player);
+  client.write('password: ');
   clientState.state = STATE.CheckPassWord;
 }
+
+
+function checkPassword(client: Socket, data: Buffer, clientState: ClientState): void {
+  if (clientState.player.isNone()) {
+    console.log('ERROR: clientState is missing player. Some logic is broken in handleData');
+    return;
+  }
+
+  const player = clientState.player.unwrap();
+  const password = data.toString().trim();
+  if (!player.password.map(p => p === password).unwrapOr(false)) {
+    client.write('Wrong password!\r\n');
+    clientState.state = STATE.CheckPassWord;
+    return;
+  }
+  client.write('password is correct!\r\n');
+  clientState.state = STATE.Connected;
+  online.push(client);
+}
+
 
 function checkToCreateUser(client: Socket, data: Buffer, clientState: ClientState): void {
   const answer = data.toString().trim();
@@ -84,12 +115,11 @@ function checkToCreateUser(client: Socket, data: Buffer, clientState: ClientStat
     return;
   }
 
-  let player = new Player();
-  clientState.player = Option.some(player);
   online.push(client)
   client.write('Set a password: ');
   clientState.state = STATE.SetPassWord;
 }
+
 
 function setPassword(client: Socket, data: Buffer, clientState: ClientState): void {
   const password = data.toString().trim();
@@ -97,9 +127,11 @@ function setPassword(client: Socket, data: Buffer, clientState: ClientState): vo
     p.password = Option.some(password)
     return p
   });
+  client.write('your password was set.\r\n');
   clientState.player = player
   clientState.state = STATE.Connected
 }
+
 
 function connected(client: Socket, data: Buffer, clientState: ClientState): void {
   if (clientState.player.isNone()) {
@@ -110,6 +142,7 @@ function connected(client: Socket, data: Buffer, clientState: ClientState): void
   const message = `[${player.name}]: ${data.toString().trim()}`;
   broadcast(message, client);
 }
+
 
 function handleEnd(client: Socket, clientState: ClientState): void {
   if (clientState.player.isNone()) {
@@ -123,8 +156,9 @@ function handleEnd(client: Socket, clientState: ClientState): void {
   }
 
   const idx = online.indexOf(client);
-  online.splice(idx);
+  online.splice(idx, 1);
 }
+
 
 function broadcast(message: string, sender: Socket) {
   online.forEach((client) => {
@@ -134,203 +168,15 @@ function broadcast(message: string, sender: Socket) {
   });
 }
 
+
 const server = net.createServer(c => createClient(c, {
   player: Option.none(),
   state: STATE.GetName
 }));
+
 
 const PORT = 8080;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-
-// type PlayerState = {
-//   client: Socket;
-//   player: Player;
-// };
-
-// class MudServer {
-//   private offline: Array<Player>;
-//   private online: Array<PlayerState>;
-//   private PORT: number;
-//   constructor() {
-//     this.PORT = 8080;
-//     this.offline = [];
-//     this.online = [];
-//   }
-
-//   listen() {
-//     console.log(`Server is listening on port ${this.PORT}`);
-//   }
-
-//   connection(client: Socket) {
-//     let state = {state: STATE.GetName};
-//     client.setEncoding('utf8');
-//     client.write('Welcome to the chat server!\r\n');
-//     client.write('Enter your name: ');
-
-//     client.on('data', data => this.handleData(client, data, state));
-//     client.on('end', () => this.handleEnd(client));
-//   }
-
-//   handleData(client: Socket, data: Buffer, state: State) {
-//     if (data.toString().charCodeAt(0) === 65533) {
-//       client.end();
-//       return;
-//     }
-//     switch (state.state) {
-//       case STATE.GetName: {
-//         const name = data.toString().trim();
-//         const idx = this.offline.findIndex(p => p.name === name);
-//         if (idx < 0) {
-//           client.write('looks like your not in the system do you want to create a character?');
-//           state.state = STATE.CheckToCreateUser;
-//           return;
-//         }
-//         break;
-//       }
-//       case STATE.GetName:{
-//         break;
-//       }
-//       case STATE.CheckPassWord:{
-//         break;
-//       }
-//       case STATE.CheckToCreateUser:{
-//         const answer = data.toString().trim();
-//         if (!['yes', 'y'].includes(answer.toLowerCase())) {
-//           client.write('Enter your name: ');
-//           state.state = STATE.GetName;
-//           return; 
-//         }
-//         let player = new Player();
-//         this.online.push({client, player})
-//         client.write('Set a password: ');
-//         state.state = STATE.SetPassWord;
-//         break;
-//       }
-//       case STATE.SetPassWord:{
-//         break;
-//       }
-//       case STATE.Connected:{
-//         break;
-//       }
-//     }
-//   }
-
-//   handleEnd(client: Socket) {
-//     const idx = this.online.findIndex(ps => ps.client === client);
-//     const [ {player} ] = this.online.splice(idx, 1);
-//     this.offline.push(player);
-//     if (player.name) {
-//       this.broadcast(`User '${player.name}' has left the chat.`, client, player);
-//     }
-//   }
-
-//   broadcast(message: string, sender: Socket, player: Player) {
-//     if (!player.hasName() || !player.hasPassword()) {
-//       return;
-//     }
-//     this.online.forEach(({client}) => {
-//       if (client !== sender) {
-//         client.write(message + '\n');
-//       }
-//     });
-//   }
-
-//   run() {
-//     const server = net.createServer((client: Socket) => this.connection(client));
-//     server.listen(this.PORT, () => this.listen());
-//   }
-
-//   mapOnline(fn: (player: PlayerState) => void) {
-//     this.online.forEach(fn);
-//   }
-// }
-
-// new MudServer()
-//   // .handleCommand(foo)
-//   .run();
-
-
-// function foo(data: Buffer) -> Command {
-  
-// }
-
-
-// const clients: Map<string, Player> = new Map();
-//
-// const server = net.createServer((client: Socket) => {
-//   client.setEncoding('utf8');
-//   client.write('Welcome to the chat server!\n');
-//
-//   let player = new Player(client);
-//
-//   if (player.hasPassword()) {
-//     client.write(`Hello ${player.name}, please enter your password: `);
-//   } else {
-//     client.write('Please enter your username: ');
-//   }
-//
-//   let loggedIn = false;
-//
-//   client.on('data', (data) => {
-//     if (data.toString().charCodeAt(0) === 65533) {
-//       client.end();
-//       return;
-//     }
-//
-//     if (!player.hasName()) {
-//       player.name = data.toString().trim();
-//       broadcast(`User '${player.name}' has joined the chat.`, client, player);
-//       client.write('Please set your password: ');
-//     } else if (!player.hasPassword()) {
-//       player.password = Option.some(data.toString().trim());;
-//       loggedIn = true;
-//     } else if (player.hasName() && player.hasPassword() && !loggedIn) {
-//       player.login(data.toString().trim());
-//       loggedIn = true;
-//     } else if (loggedIn) {
-//       const message = `[${player.name}]: ${data.toString().trim()}`;
-//       broadcast(message, client, player);
-//     }
-//   });
-//
-//   client.on('end', () => {
-//     console.log(player, clientId);
-//     removeClient(client);
-//     knownClients.delete(clientId);
-//     knownClients.set(player.name, player);
-//     console.log(knownClients.get(clientId));
-//     // Notify all clients that the user has left
-//     if (player.name) {
-//       broadcast(`User '${player.name}' has left the chat.`, client, player);
-//     }
-//   });
-//
-//   clients.push(client);
-// });
-//
-// function broadcast(message: string, sender: Socket, player: Player) {
-//   if (!player.hasName() || !player.hasPassword()) {
-//     return;
-//   }
-//   clients.forEach((client) => {
-//     if (client !== sender) {
-//       client.write(message + '\n');
-//     }
-//   });
-// }
-//
-// function removeClient(client: Socket) {
-//   const index = clients.indexOf(client);
-//   if (index !== -1) {
-//     clients.splice(index, 1);
-//   }
-// }
-//
-// const PORT = 8080;
-// server.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// });
-//
