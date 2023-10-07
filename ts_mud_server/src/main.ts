521983f4a7767d82ccde7b1038a8c42a7e624859
cowboy8625 <cowboy8625@yahoo.com>
2023-10-07 11:19:39 -0500

    changed release port name

--- ts_mud_server/src/main.ts
+++ ts_mud_server/src/main.ts
@@ -7,7 +7,7 @@ import { System, Entity, createId, queryEntities } from "./ecs/mod";
 
 const app = express();
 const { app: wsApp } = expressWs(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.ELDORIA_PORT || 3000;
 
 type Message = {
   message: string;
